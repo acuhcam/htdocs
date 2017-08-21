51ae3fe73691f2401b62a3252d25833a4b282670
Akihiko_Tsushima <akihiko_tsushima@yahoo.co.jp>
2017-08-21 16:09:58 +0900

    名前空間　途中まで

---
+++ objective-php/index12.php
@@ -0,0 +1,20 @@
+<?php
+
+//名前空間
+namespace FoodShop;
+
+//ProductというクラスはFoodShopという名前空間に属することになる
+class Product
+{
+
+}
+
+//--------------------------------
+
+use FoodShop\Product;
+
+$foodProduct = new Product();
+
+
+
+ ?>
