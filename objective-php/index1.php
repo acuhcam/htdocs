61b016665a2488f80a18c75b0971a55853ea2c6b
Akihiko_Tsushima <tsushima@respawn.tokyo>
2017-08-21 10:28:00 +0900

    オブジェクト指向クラス

---
+++ objective-php/index1.php
@@ -0,0 +1,29 @@
+<?PHP
+
+
+class Product {
+  private $name; //商品名
+  private $price; //価格
+
+  //商品名を取得する
+  public function getName(){
+    return $this->name;
+  }
+
+  //商品名を設定する
+  public function setName($name){
+    $this->name = $name;
+  }
+
+
+
+
+}
+
+
+
+
+
+
+
+?>
